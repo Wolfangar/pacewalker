0aa2e70769b8e07e43165859a1a75190c94b2a64
Wolfangar <raphael.bailly@etudiant.cnam-enjmin.fr>
2017-10-06 23:18:58 +0200

    Parallax for the win

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+    public float parallaxSpeed;
+
+    private float deltaX, lastCameraX;
+    private Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+        cameraTransform = Camera.current.transform;
+        lastCameraX = cameraTransform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        deltaX = cameraTransform.position.x - lastCameraX;
+        transform.position += Vector3.right * (deltaX * parallaxSpeed);
+        lastCameraX = cameraTransform.position.x;
+    }
+}
