294f985e6b582e75f5dc3ed562ef8577c52dd4ca
tabasscore <antonin.demeilliez@gmail.com>
2017-10-06 23:00:18 +0200

    camera+controller (début)

---
+++ Assets/cameracontroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour {
+
+	public GameObject controller;
+	public bool camtrigger;
+	public float offsety;
+	public float xblock;
+
+
+	// Use this for initialization
+	void Start () {
+
+		camtrigger = controller.GetComponent<characontroller>().camtrigger;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector2 (controller.transform.position.x, controller.transform.position.y + offsety) ;
+
+
+		if (camtrigger == true){
+
+
+					gameObject.transform.position = new Vector2 (xblock, controller.transform.position.y + offsety) ;
+
+
+		}
+
+	}
+
+	
+}
---
+++ Assets/characontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characontroller : MonoBehaviour {
+
+	public bool camtrigger;
+	public float xblock;
+	// Use this for initialization
+	void Start () {
+		camtrigger = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		camtrigger = true;
+		xblock = gameObject.transform.position.x;
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		camtrigger = false;
+	}
+
+}
