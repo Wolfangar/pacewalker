3ca1a02ebd321f1bcb12e3f95f59f6f1b1b1ea58
Wolfangar <raphael.bailly@etudiant.cnam-enjmin.fr>
2017-10-07 14:49:32 +0200

    Dash + movement clean

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
     public GameObject target;
@@ -31,20 +32,27 @@ public class Enemy : MonoBehaviour {
             }
         }
     */
+        GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+        GetComponent<NavMeshAgent>().updateRotation = false;
     }
 
     void FixedUpdate()
     {
+        /*
+
+        Vector2 le = new Vector2(0, 0.0f);
+        Vector2 newPos = new Vector2(transform.position.x, transform.position.y - GetComponent<SpriteRenderer>().bounds.size.y / 2) + le;
+
+
         Vector2 direction = (target.transform.position - transform.position).normalized;
 
+        
+        rigidBody.MovePosition(rigidBody.position + direction * speed * Time.fixedDeltaTime);
 
         
-
-        rigidBody.MovePosition(rigidBody.position + direction * speed * Time.fixedDeltaTime);
-        Vector2 le = new Vector2(0, 0.2f);
-        Vector2 newPos = new Vector2(transform.position.x, transform.position.y - GetComponent<SpriteRenderer>().bounds.size.y / 2) + le;
-        RaycastHit2D hit = Physics2D.Raycast(newPos, direction, 2);
+        RaycastHit2D hit = Physics2D.Raycast(newPos, direction, 2.0f);
         Debug.DrawRay(newPos, direction * 2, Color.green);
+
         if (hit.collider != null)
         {
             Debug.Log("hit");
@@ -53,12 +61,21 @@ public class Enemy : MonoBehaviour {
             vv.x = direction.y;
             vv.y = -direction.x;
 
-            Debug.DrawRay(newPos, vv * 2, Color.blue);
-            if (hit.collider != null)
+            Vector2 direction2 = ((Vector2) target.transform.position - newPos).normalized;
 
-                rigidBody.AddForce(vv * 20 * (2 - hit.distance));
+            float angle = Mathf.Atan2(direction2.y, direction2.x) * Mathf.Rad2Deg;
+            int leftRight = angle > 0 ? -1 : 1;
+
+            Debug.DrawRay(newPos, vv * 2, Color.blue);
+            Debug.DrawRay(newPos, leftRight * vv * 2, Color.red);
+            //rigidBody.AddForce(leftRight * vv * 20 * Mathf.Pow((2 - hit.distance), 2));
+            //rigidBody.AddForce(-direction * 10 * Mathf.Pow((2 - hit.distance), 2));
+
+
+
             //rigidBody.AddForce((new Vector3(hit.point.x, hit.point.y, transform.position.z) - transform.position).normalized * -20 * (2 - hit.distance));
             //rigidBody.AddForce(new Vector3(Random.Range(-1, 2), Random.Range(-1, 2), 0) * -10 * (2 - hit.distance));
         }
+        */
     }
 }
---
+++ Assets/Scripts/characontroller3D.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characontroller3D : MonoBehaviour {
+
+	public bool camtrigger;
+	public float xblock;
+	public float speed;
+	public float dash;
+	public float dashspeed;
+	public float xmove;
+	public float ymove;
+	public bool recover;
+
+    private Rigidbody rigidBody;
+
+    protected Vector3 dashTarget;
+    protected float dashSpeed = 20;
+    public float dashDistance = 3;
+
+    // Use this for initialization
+    void Start () {
+		camtrigger = false;
+		dash = 0;
+		recover = true;
+
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+
+    private bool isDashing = false;
+
+    private void Dash()
+    {
+        Vector3 direction = velocity.normalized;
+
+        isDashing = true;
+
+        Debug.Log("direction: " + direction);
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, direction);
+        Physics.Raycast(ray, out hit, dashDistance);
+        Debug.DrawRay(transform.position, direction * dashDistance, Color.green, 5.0f);
+        if (hit.collider != null)
+        {
+            Debug.Log("hit" + hit.distance);
+            float distanceReal = Vector3.Distance(transform.position, hit.point);
+            dashTarget = transform.position + (Vector3)((direction * dashDistance)
+                * ((distanceReal - 0.3f) / dashDistance) );
+        }
+        else
+        {
+            dashTarget = transform.position + (Vector3)(direction * dashDistance);
+        }
+        rigidBody.velocity = Vector3.zero;
+        velocity = Vector3.zero;
+    }
+
+    private Vector3 velocity;
+
+    private void Update()
+    {
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isDashing)
+        {
+            Dash();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (isDashing)
+        {
+            rigidBody.velocity = Vector2.zero;
+
+            float distSqr = (dashTarget - transform.position).sqrMagnitude;
+            if (distSqr < 0.1f)
+            {
+                isDashing = false;
+                dashTarget = Vector2.zero;
+            }
+            else
+            {
+                Debug.Log("dashing");
+                rigidBody.MovePosition(Vector3.Lerp(transform.position, dashTarget, dashSpeed * Time.deltaTime));
+            }
+        }
+        else
+        {
+            rigidBody.velocity = Vector2.zero;
+            //Debug.Log("force is there" + velocity);
+            rigidBody.AddForce(velocity, ForceMode.VelocityChange);
+        }
+
+        /*
+        if (!recover)//isDashing
+        {
+
+        }
+        else
+        {
+
+        }
+
+		if (Input.GetKeyDown("space") && recover == true)
+		{
+			recover = false;
+			dash = dashspeed;
+			xmove = Input.GetAxisRaw("Horizontal");
+			ymove = Input.GetAxisRaw("Vertical");
+
+
+            //rigidBody.velocity = new Vector3(xmove, 0.0f, ymove).normalized * dashspeed;
+            rigidBody.MovePosition(transform.position + new Vector3(xmove, 0.0f, ymove).normalized * dashspeed);
+            //rigidBody.AddForce(new Vector3(xmove, 0.0f, ymove).normalized * 50, ForceMode.VelocityChange);
+
+            StartCoroutine(Wait());
+
+            return;
+        }
+		else
+			{
+			dash = 0;
+			xmove = Input.GetAxisRaw("Horizontal");
+			ymove = Input.GetAxisRaw("Vertical");
+		}
+        Vector3 newDirection = new Vector3(xmove, 0.0f, ymove).normalized * speed * Time.deltaTime;
+
+        transform.Translate(newDirection);
+        //rigidBody.MovePosition(transform.position + newDirection);
+        */
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		camtrigger = true;
+		xblock = gameObject.transform.position.x;
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		camtrigger = false;
+	}
+
+	IEnumerator Wait()
+	{
+		
+		yield return new WaitForSeconds(0.5f);
+		recover = true;
+	
+	}
+
+}
