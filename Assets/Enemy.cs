59387a64b0aef3e074541aa2a21f1062ca9b23f9
Wolfangar <raphael.bailly@etudiant.cnam-enjmin.fr>
2017-10-07 11:31:54 +0200

    Stamina

---
+++ Assets/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public GameObject target;
+    public float speed;
+
+    private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 direction = (target.transform.position - transform.position).normalized;
+        //transform.position += direction * speed * Time.deltaTime;
+        //rigidBody.AddForce(direction * speed * Time.deltaTime);
+        Vector2 newPos = new Vector2(transform.position.x, transform.position.y - GetComponent<SpriteRenderer>().bounds.size.y / 2);
+        /*
+        RaycastHit2D hit = Physics2D.Raycast(newPos, direction, 2);
+        Debug.DrawRay(newPos, direction * 2, Color.green);
+        if (hit.collider != null)
+        {
+            if(Random.Range(0, 2) == 0)
+            {
+                //rigidBody.AddForce(Vector3.up * 10);
+                //direction += Vector2.up * 10;
+            }
+        }
+    */
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction = (target.transform.position - transform.position).normalized;
+
+
+        
+
+        rigidBody.MovePosition(rigidBody.position + direction * speed * Time.fixedDeltaTime);
+        Vector2 le = new Vector2(0, 0.2f);
+        Vector2 newPos = new Vector2(transform.position.x, transform.position.y - GetComponent<SpriteRenderer>().bounds.size.y / 2) + le;
+        RaycastHit2D hit = Physics2D.Raycast(newPos, direction, 2);
+        Debug.DrawRay(newPos, direction * 2, Color.green);
+        if (hit.collider != null)
+        {
+            Debug.Log("hit");
+            //rigidBody.AddForce(Vector3.up * 10);
+            Vector2 vv;
+            vv.x = direction.y;
+            vv.y = -direction.x;
+
+            Debug.DrawRay(newPos, vv * 2, Color.blue);
+            if (hit.collider != null)
+
+                rigidBody.AddForce(vv * 20 * (2 - hit.distance));
+            //rigidBody.AddForce((new Vector3(hit.point.x, hit.point.y, transform.position.z) - transform.position).normalized * -20 * (2 - hit.distance));
+            //rigidBody.AddForce(new Vector3(Random.Range(-1, 2), Random.Range(-1, 2), 0) * -10 * (2 - hit.distance));
+        }
+    }
+}
---
+++ Assets/Scripts/StaminaManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaManager : MonoBehaviour {
+    public int totalHealth, hitDamage, dashStamina;
+    public float decreasingSpeed, currentHealth;
+
+    private void Start()
+    {
+        currentHealth = totalHealth;
+    }
+
+    private void Update()
+    {
+        currentHealth -= decreasingSpeed * Time.deltaTime;
+        currentHealth = Mathf.Clamp(currentHealth, 0.0f, totalHealth);
+        checkHealth();
+    }
+
+    private void checkHealth()
+    {
+        if (currentHealth <= 0.0f)
+            dead();
+    }
+
+    public void dead()
+    {
+        Debug.Log(transform.name + " is dead, not a big surprise");
+        Destroy(this);
+        //Time.timeScale = 0;
+    }
+
+    public void loseHealth(float damage)
+    {
+        float newHealth = currentHealth - damage;
+        currentHealth = Mathf.Clamp(newHealth, 0.0f, totalHealth);
+        checkHealth();
+    }
+
+    public void getHealth(float health)
+    {
+        float newHealth = currentHealth + health;
+        //currentHealth += newHealth > totalHealth ? totalHealth : newHealth;
+        currentHealth = Mathf.Clamp(newHealth, 0.0f, totalHealth);
+    }
+
+    /*
+    public int segments, bars; //number of segment and "under"-segment: bar
+    public int healthPerBar;
+    public float decreasingSpeed;
+
+    private int currentSegment, currentBar;
+    private float totalHealth, currentHealth;
+
+	// Use this for initialization
+	void Start () {
+        totalHealth = currentHealth = segments * bars * healthPerBar;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentHealth -= decreasingSpeed * Time.deltaTime;
+        checkHealth();
+
+
+        currentSegment = (int) (currentHealth / (bars * healthPerBar) - 1) + 1;
+        (posX / sizeX) * sizeX
+
+
+        currentBar = (int)currentHealth / (segments * bars);
+    }
+
+    void checkHealth()
+    {
+        if (totalHealth <= 0)
+        {
+            dead();
+        }
+    }
+
+    void dead()
+    {
+        Debug.Log("dead, not a big surprise");
+    }
+
+    public void loseBar()
+    {
+        //currentHealth -= 
+        checkHealth();
+    }
+
+    public void loseSegment()
+    {
+        checkHealth();
+    }
+
+    public void getHealth(float health)
+    {
+        totalHealth += health;
+    }
+    */
+}
