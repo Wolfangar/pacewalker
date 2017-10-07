360ab5d1e739f6d944cfa5665190f1f3827f5ed4
tabasscore <antonin.demeilliez@gmail.com>
2017-10-07 13:46:29 +0200

    variable publique cooldown

--- Assets/Scripts/characontroller.cs
+++ Assets/Scripts/characontroller.cs
@@ -12,6 +12,7 @@ public class characontroller : MonoBehaviour {
 	public float xmove;
 	public float ymove;
 	public bool recover;
+	public float cooldown;
 	StaminaManager staminascript;
 
 	// Use this for initialization
@@ -62,7 +63,7 @@ public class characontroller : MonoBehaviour {
 	IEnumerator Wait()
 	{
 		
-		yield return new WaitForSeconds(3.0f);
+		yield return new WaitForSeconds(cooldown);
 		recover = true;
 	
 	}
