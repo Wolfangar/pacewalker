dc245ef215b5427fa541ca91c906bf00a7d9c535
tabasscore <antonin.demeilliez@gmail.com>
2017-10-08 13:14:38 +0200

    son piles + nouveaux mp3

--- Assets/Scripts/miamunepile.cs
+++ Assets/Scripts/miamunepile.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class miamunepile : MonoBehaviour {
 
 	public float petitepile, grossepile;
+	AudioSource src;
+	public AudioClip sonpetitepile, songrossepile;
+	 
+	
 	// Use this for initialization
 
     private StaminaManager stam;
 
+
+
 	void Start () {
         stam = GetComponent<StaminaManager>();
+		src = GetComponent<AudioSource>();
 
     }
 	
@@ -23,13 +30,15 @@ public class miamunepile : MonoBehaviour {
 	{
 		if(coll.gameObject.tag == "petite_pile")
 		{
+			src.PlayOneShot(sonpetitepile);
 			Debug.Log("touche une petite pile");
             stam.currentHealth += petitepile;
 			Destroy(coll.gameObject);
 		}
 		if (coll.gameObject.tag == "grosse_pile")
 		{
-            stam.currentHealth += grossepile;
+			src.PlayOneShot(songrossepile);
+			stam.currentHealth += grossepile;
 			Destroy(coll.gameObject);
 		}
 	}
